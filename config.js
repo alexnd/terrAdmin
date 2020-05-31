bf90e6a448e4e1c0687dd14233b2aedf63c5bd7b
Aleksandr Melanchenko <alexnd@gmail.com>
2020-05-31 04:08:12 +0300

    set default linux config

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 module.exports = {
   httpPort: 7878,
   httpAddr: '0.0.0.0',
+
   // mac
   // terrariaDir: './TerrariaServer/1404/Mac/Terraria Server.app/Contents/MacOS',
-  // terrariaExe: 'TerrariaServer.exe',
 
   // linux
-  // terrariaDir: 'TerrariaServer/1404/Linux',
-  // terrariaExe: 'TerrariaServer.exe',
+  terrariaDir: './TerrariaServer/1404/Linux',
 
   // windows
   // terrariaDir: 'TerrariaServer/1404/Windows',
-  // terrariaExe: 'TerrariaServer.exe',
 
   // tshock
-  terrariaDir: './TerrariaServer/tshock',
+  // terrariaDir: './TerrariaServer/tshock',
+
   terrariaExe: 'TerrariaServer.exe',
 
   sessionSecret: 'fooBar42',
