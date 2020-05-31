e1414886c495bfaf48a38cc1b8a0ba725e2b7388
Aleksandr Melanchenko <alexnd@gmail.com>
2020-05-31 19:54:28 +0300

    improve

--- postinstall.js
+++ postinstall.js
@@ -2,7 +2,6 @@
 // (c) 2020 Alexnd
 
 var fs = require('fs');
-// var path = require('path');
 
 if (!fs.existsSync('./config-local.js')) fs.copyFileSync('./config.js', './config-local.js');
 if (!fs.existsSync('./server.cfg')) fs.copyFileSync('./server-example.cfg', './server.cfg');
