895dc04172d9ded19d0960bf71e744384ba7e135
Aleksandr Melanchenko <alexnd@gmail.com>
2020-10-24 23:28:21 +0300

    update server.sh

--- server.sh
+++ server.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 #mono ./TerrariaServer/1411/Mac/Terraria\ Server.app/Contents/MacOS/TerrariaServer.exe -config ./server.cfg
-mono ./TerrariaServer/Tshock/TerrariaServer.exe -config ./server.cfg
+#mono ./TerrariaServer/Tshock/TerrariaServer.exe -config ./server.cfg
+mono ./TerrariaServer/1411/Linux/TerrariaServer.exe -config ./server.cfg
